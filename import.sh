9c2417a219e7c8f5c935baa59ff600a83afcab1e
Frank Celler <f.celler@triagens.de>
2013-04-24 18:40:10 +0200

    fixed path

--- import.sh
+++ import.sh
@@ -28,7 +28,7 @@ done
 echo "Importing 10000 fake users into 'users'"
 echo "======================================="
 (echo 'db._drop("users");' | $ARANGOSH -s) || exit 1
-$ARANGOIMP --file RandomUsers/names_10000.json --collection=users --create-collection=true --type=json || exit 1
+$ARANGOIMP --server.password "" --file RandomUsers/names_10000.json --collection=users --create-collection=true --type=json || exit 1
 (echo 'db.users.ensureHashIndex("name.first"); db.users.ensureHashIndex("name.last");' | $ARANGOSH -s) || exit 1
 (echo 'db.users.ensureHashIndex("contact.address.city"); db.users.ensureHashIndex("contact.address.zip");' | $ARANGOSH -s) || exit 1
 echo
@@ -37,7 +37,7 @@ echo
 echo "Importing cities into 'cities'"
 echo "=============================="
 (echo 'db._drop("cities");' | $ARANGOSH -s) || exit 1
-$ARANGOIMP --file Cities/GeoLiteCity.csv --collection=cities --create-collection=true --type=csv || exit 1
+$ARANGOIMP --server.password "" --file Cities/GeoLiteCity.csv --collection=cities --create-collection=true --type=csv || exit 1
 (echo 'db.users.ensureGeoIndex("latitude", "longitude");' | $ARANGOSH -s) || exit 1
 (echo 'db.users.ensureHashIndex("city");' | $ARANGOSH -s) || exit 1
 echo
@@ -46,7 +46,7 @@ echo
 echo "Importing countries into 'countries'"
 echo "===================================="
 (echo 'db._drop("countries");' | $ARANGOSH -s) || exit 1
-$ARANGOIMP --file Countries/countries.csv --collection=countries --create-collection=true --type=csv || exit 1
+$ARANGOIMP --server.password "" --file Countries/countries.csv --collection=countries --create-collection=true --type=csv || exit 1
 (echo 'db.users.ensureHashIndex("name");' | $ARANGOSH -s) || exit 1
 echo
 echo
@@ -54,7 +54,7 @@ echo
 echo "Importing regions into 'regions'"
 echo "================================"
 (echo 'db._drop("regions");' | $ARANGOSH -s) || exit 1
-$ARANGOIMP --file Regions/regions.csv --collection=regions --create-collection=true --type=csv || exit 1
+$ARANGOIMP --server.password "" --file Regions/regions.csv --collection=regions --create-collection=true --type=csv || exit 1
 (echo 'db.users.ensureHashIndex("name");' | $ARANGOSH -s) || exit 1
 echo
 echo
@@ -62,7 +62,7 @@ echo
 echo "Importing McDonalds France into 'mcdonalds'"
 echo "==========================================="
 (echo 'db._drop("mcdonalds");' | $ARANGOSH -s) || exit 1
-$ARANGOIMP --file McDonalds/france.csv --collection=mcdonalds --create-collection=true --type=csv || exit 1
+$ARANGOIMP --server.password "" --file McDonalds/france.csv --collection=mcdonalds --create-collection=true --type=csv || exit 1
 (echo 'db.users.ensureGeoIndex("lat", "long");' | $ARANGOSH -s) || exit 1
 echo
 echo
@@ -70,7 +70,7 @@ echo
 echo "Importing German Counties into 'bezirke'"
 echo "========================================"
 (echo 'db._drop("bezirke");' | $ARANGOSH -s) || exit 1
-$ARANGOIMP --file Bezirke/bezirke.csv --collection=bezirke --create-collection=true --type=csv || exit 1
+$ARANGOIMP --server.password "" --file Bezirke/bezirke.csv --collection=bezirke --create-collection=true --type=csv || exit 1
 (echo 'db.users.ensureGeoIndex("lat", "long");' | $ARANGOSH -s) || exit 1
 (echo 'db.users.ensureHashIndex("FULL_NAME_RO");' | $ARANGOSH -s) || exit 1
 echo
@@ -79,7 +79,7 @@ echo
 echo "Importing Airports into 'airports'"
 echo "=================================="
 (echo 'db._drop("airports");' | $ARANGOSH -s) || exit 1
-$ARANGOIMP --file Airports/airports.csv --collection=airports --create-collection=true --type=csv || exit 1
+$ARANGOIMP --server.password "" --file Airports/airports.csv --collection=airports --create-collection=true --type=csv || exit 1
 (echo 'db.users.ensureGeoIndex("latitude_deg", "longitude_deg");' | $ARANGOSH -s) || exit 1
 (echo 'db.users.ensureHashIndex("name");' | $ARANGOSH -s) || exit 1
 echo
@@ -88,7 +88,7 @@ echo
 echo "Importing NerdPursuit into 'nerds'"
 echo "=================================="
 (echo 'db._drop("nerds");' | $ARANGOSH -s) || exit 1
-./nerd_pursuit_compress.sh
-$ARANGOIMP --file nerd_pursuit_compressed.json --collection=nerds --create-collection=true --type=json || exit 1
+(cd NerdPursuit && ./nerd_pursuit_compress.sh)
+(cd NerdPursuit && $ARANGOIMP --server.password "" --file nerd_pursuit_compressed.json --collection=nerds --create-collection=true --type=json) || exit 1
 echo
 echo
