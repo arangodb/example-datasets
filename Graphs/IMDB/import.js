afa948123858a5e7eb1ec5a688ca15b47ef1f412
Simran <Simran-B@users.noreply.github.com>
2021-04-30 21:20:25 +0200

    IMDB: Update import script, source files and dump (#12)
    * Update script and source files
    - Removed duplicate edges
    - Removed empty fields ("")
    - Removed a dummy edge
    - Converted releaseDate to number, corrected timezone (was apparently assuming Europe/Berlin instead of UTC)
    - Fixed 13 releaseDates manually
    - Make the script work with ArangoDB 3.7/3.8
    - Don't drop edge _key anymore (duplicates removed in source file)
    - Add type: "Genre" to genre vertices
    - Disable creation of fulltext indexes
    * Update README
    * Change source file extensions from .json to .jsonl
    * Update dump

--- Graphs/IMDB/import.js
+++ Graphs/IMDB/import.js
@@ -10,15 +10,21 @@ var fs=require("fs");
   var gm = require('@arangodb/general-graph');
   var g;
   try {
-    g = gm._create(gName);
-    g.drop();
-  } catch (e) {
-
-  }
+    db._drop(vName);
+  } catch (e) { }
+  try {
+    db._drop(eName);
+  } catch (e) { }
+  try {
+    gm._drop(gName);
+  } catch (e) { }
+  db._create(vName);
+  db._createEdgeCollection(eName);
   g = gm._create(gName);
   g._addVertexCollection(vName);
   var rel = gm._relation(eName, vName, vName);
   g._extendEdgeDefinitions(rel);
+  g = gm._graph(gName);
   var genres = {};
   
   var toKey = function(d) {
@@ -27,42 +33,45 @@ var fs=require("fs");
 
   var storeVertex = function(d) {
     if (d.releaseDate) {
-      var datF = new Date(parseInt(d.releaseDate));
-      datF = datF.getFullYear();
+      var datF = new Date(d.releaseDate).toISOString().slice(0, 4);
       datF = Math.floor(datF / 10) * 10;
       datS = datF + 10;
       var dat = datF + "-" + datS;
       d.released = dat;
     }
-    g.addVertex(d._key, d);
+    g[vName].save(d);
     if (d.genre) {
       var gK = toKey(d.genre);
       if (!genres[gK]) {
         genres[gK] = true;
-        g.addVertex(gK, {label: d.genre});
+        g[vName].save({_key: gK, label: d.genre, type: "Genre"});
       }
-      g.addEdge(gK, d._key, null, "has_movie");
+      g[eName].save({
+        _from: vName + "/" + gK,
+        _to: vName + "/" + d._key,
+        $label: "has_movie"
+      });
     }
   };
 
   var storeEdge = function(d) {
-    var to = d._to;
-    var from = d._from;
+    d._from = vName + "/" + d._from;
+    d._to = vName + "/" + d._to;
 
-    delete d._key;
     try {
-      g.addEdge(from, to, null, d["$label"], d);
+      g[eName].save(d);
     } catch (e) {
-      console.log("Failed:", from, "->", to);
+      console.log("Failed:", d._from, "->", d._to);
     }
   };
 
   var verticesCollection = db._collection(vName);
-  verticesCollection.ensureFulltextIndex("description", 3);
-  verticesCollection.ensureFulltextIndex("title", 3);
-  verticesCollection.ensureFulltextIndex("name", 3);
-  verticesCollection.ensureFulltextIndex("birthplace", 3);
+  // Leave index creation up to the user, who may prefer an ArangoSearch View
+  //verticesCollection.ensureFulltextIndex("description", 3);
+  //verticesCollection.ensureFulltextIndex("title", 3);
+  //verticesCollection.ensureFulltextIndex("name", 3);
+  //verticesCollection.ensureFulltextIndex("birthplace", 3);
 
-  internal.processJsonFile(fs.join(__dirname, "nodes.json"), storeVertex);
-  internal.processJsonFile(fs.join(__dirname, "edges.json"), storeEdge);
+  internal.processJsonFile(fs.join(__dirname, "nodes.jsonl"), storeVertex);
+  internal.processJsonFile(fs.join(__dirname, "edges.jsonl"), storeEdge);
 }());
